f8ddd863a7c5fedf9afb43b6f6fc234ff00bc057
afwani-git <afwani321@gmail.com>
2021-02-18 19:43:46 +0700

    fixing e2e example

--- test/app.e2e-test.js
+++ test/app.e2e-test.js
@@ -3,20 +3,20 @@ const app = require("../app");
 const db = require("../app/models");
 
 describe("testing api e2e", () => {
-  describe("endpoint /", () => {
+  describe("endpoint /api/", () => {
     it("should have return 200 OK if params even", async () => {
-      await request(app).get("/2").expect(200);
+      await request(app).get("/api/2").expect(200);
     });
 
     it("should have return 201 if params odd", async () => {
-      await request(app).get("/1").expect(201);
+      await request(app).get("/api/1").expect(201);
     });
   });
 
   describe("endpoint /user", () => {
     it("should have return result", async () => {
       await request(app)
-        .get("/user")
+        .get("/api/user")
         .expect(200)
         .then((res) => {
           expect(res.body).toEqual({

341570c5abe3ac1475f6a4b093a285a228ee7d58
afwani-git <afwani123@gmail.com>
2021-03-11 19:34:33 +0700

    make a genToken helper and make controller for fetch remote api with axios

--- app/deliverey/http/routers.js
+++ app/deliverey/http/routers.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const homeController = require("./controllers/homeController");
 
+router.get("/axios/tes", homeController.tesFetchData)
 router.get("/user/fetch", middlewareExample, homeController.fetchUser);
 router.get("/redis/tes/:key",homeController.testRedis)
 router.get("/:number", homeController.index);
--- app/services/homeService.js
+++ app/services/homeService.js
@@ -1,4 +1,7 @@
-const userModel = require("../models/users");
+const userModel = require("../models/users")
+const axios = require("axios").default
+const jwt = require("../../app/helpers/genToken")  
+require('dotenv').config()
 
 const fetchUser = async () => {
   return userModel.find();
@@ -12,6 +15,27 @@ const testRedis = async () => {
   })
 }
 
+const testFetchData = async () => {  
+  try{
+
+    const MOCK_URL = process.env.MOCK_URL
+    const { token } = jwt.genToken()
+    
+    const result = await axios.post(`${MOCK_URL}/mocks/v1/personals/coverage/check`, {}, {
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "X-Access-Host": `${MOCK_URL}`,
+        "Content-Type": "application/json",
+        "accept": "application/json"
+      }
+    })
+
+    return result.data;
+  }catch(err){
+    throw err
+  }
+}
+
 const checkNumber = (number) => {
   if (number % 2 == 0) return true;
 
@@ -22,4 +46,5 @@ module.exports = {
   fetchUser,
   checkNumber,
   testRedis,
+  testFetchData
 };

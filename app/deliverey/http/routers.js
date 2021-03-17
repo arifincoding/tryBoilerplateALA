67e3da5789494d124b388799ec208696956f0ce0
arifincoding <arifinnakpojok@gmail.com>
2021-03-17 16:49:59 +0700

    hapus cobacoba

--- app/deliverey/http/routers.js
+++ app/deliverey/http/routers.js
@@ -14,10 +14,4 @@ router.post("/user/insert",userController.insertUser);
 router.put("/user/update",userController.updateUser)
 router.get("/user/get/:username",userController.getByUsername)
 router.delete("/user/delete/:username",userController.deleteByUsername)
-router.get("/user/coba",(req, res)=>{
-    res.send("hai coba")
-})
-router.get("/user/cobacoba",(req,res)=>{
-    res.send("cobacoba");
-})
 module.exports = router;

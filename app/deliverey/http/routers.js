6917ab1b585dbc6e68ee07c3f8dec0519ea12aec
arifincoding <arifinnakpojok@gmail.com>
2021-03-17 16:43:12 +0700

    cobacoba paralel

--- app/deliverey/http/routers.js
+++ app/deliverey/http/routers.js
@@ -14,4 +14,7 @@ router.post("/user/insert",userController.insertUser);
 router.put("/user/update",userController.updateUser)
 router.get("/user/get/:username",userController.getByUsername)
 router.delete("/user/delete/:username",userController.deleteByUsername)
+router.get("/user/cobacoba",(req,res)=>{
+    res.send("cobacoba");
+})
 module.exports = router;
